b69bd75da6b7982b1e548dcfb61c0b1c5ffbf6ed
0ne_Up <oneup1@gmail.com>
2020-06-08 10:40:56 +0200

    - Calc() method.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,17 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Bibliotheek
 {
@@ -45,7 +34,84 @@ namespace Bibliotheek
 
         private void Calc()
         {
+            // Valideer boeknummer.
+            string isbn = tbIsbn.Text;
+            try
+            {
+                if (isbn.Length != 4)
+                {
+                    throw new FormatException("Invalid ISBN");
+                }
+                int.Parse(isbn);
+            }
+            catch
+            {
+                MessageBox.Show("Het boeknummer moet bestaan uit 4 cijfers", "Bibliotheek",
+                    MessageBoxButton.OK, MessageBoxImage.Error);
+                tbIsbn.Focus();
+                return;
+            }
 
+            // Valideer uitleendatum/retourdatum.
+            if (dpRentalDate.SelectedDate == null)
+            {
+                dpRentalDate.Focus();
+                return;
+            }
+            if (dpReturnDate.SelectedDate == null)
+            {
+                dpReturnDate.Focus();
+                return;
+            }
+            DateTime rentalDate = dpRentalDate.SelectedDate.Value;
+            DateTime returnDate = dpReturnDate.SelectedDate.Value;
+
+            // Bepaal aantal dagen.
+            double days = (returnDate - rentalDate).TotalDays;
+            if (days <= 0)
+            {
+                MessageBox.Show("De retourdatum moet na de uitleendatum liggen", "Bibliotheek",
+                    MessageBoxButton.OK, MessageBoxImage.Error);
+                dpReturnDate.Focus();
+                return;
+            }
+
+            // Bereken aantal dagen te laat en eventuele boete.
+            double daysOverdue, fine = 0;
+            if (isbn[0] == '9')
+            {
+                // Romans: Als dagen > 21 een boete van € 0,25 per dag.
+                daysOverdue = days - 21;
+                if (daysOverdue > 0)
+                {
+                    fine = daysOverdue * 0.25;
+                }
+            }
+            else
+            {
+                // Studieboeken: Als dagen > 30 een boete van € 1,= per week.
+                // Een deel van een week telt hierbij als volle week:
+                // stel dagen = 39, dus 9 dagen te laat, 2 x € 1,= boete.
+                daysOverdue = days - 30;
+                if (daysOverdue > 0)
+                {
+                    fine = Math.Ceiling(daysOverdue / 7);
+                }
+            }
+
+            // Toon resultaat.
+            string caption = days + " dagen";
+            if (fine == 0)
+            {
+                MessageBox.Show("Geen boete!", caption,
+                    MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else
+            {
+                MessageBox.Show(daysOverdue + " dagen te laat, € " +
+                    fine.ToString("0.00") + " boete!", caption,
+                    MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
         }
     }
 }
